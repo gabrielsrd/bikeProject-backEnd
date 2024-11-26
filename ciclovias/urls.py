8bb9443689d6d99840aa167a54a6c4d85ce42075
Gabriel Alves <alves.gabriel@novaenergia.com.br>
2024-11-25 23:12:03 -0300

    showing ciclovias

--- ciclovias/urls.py
+++ ciclovias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CicloviasAPIView
+from .views import CicloStationsAPIView, CicloviasAPIView
 
 urlpatterns = [
+    path('ciclostation/', CicloStationsAPIView.as_view(), name='ciclostation'),
     path('ciclovias/', CicloviasAPIView.as_view(), name='ciclovias'),
 ]
